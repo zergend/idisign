42c8b4239647f410d7e5deeb01f57761275497dd
zergend <zergend@gmail.com>
2023-03-28 15:52:42 +0300

    settings for webpack / start

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const filename = (ext) =>
+  isDev ? `[name].${ext}` : `[name].[contenthash].${est}`;
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: "./js/main.js",
+  output: {
+    filename: `./js/${filename("js")}`,
+    path: path.resolve(__dirname, "app"),
+  },
+};
